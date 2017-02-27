7237e782f6c9243d7e96c661d32ac1ea35ab0f9a
Shelin <1332109343@qq.com>
2017-02-27 16:33:50 +0800

    fix

--- XLVideoPlayer/ViewControllers/ExampleViewController.m
+++ XLVideoPlayer/ViewControllers/ExampleViewController.m
@@ -143,7 +143,7 @@
 - (void)scrollViewDidScroll:(UIScrollView *)scrollView {
     if ([scrollView isEqual:self.exampleTableView]) {
         
-        [_player playerScrollIsSupportSmallWindowPlay:YES];
+        [_player playerScrollIsSupportSmallWindowPlay:NO];
     }
 }
 
